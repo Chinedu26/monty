8d515bcc98d3c1c72ecddee56a13c03404075a20
Chinedu26 <106815468+Chinedu26@users.noreply.github.com>
2022-10-09 18:14:05 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 bus_t bus = {NULL, NULL, NULL, 0};
 /**
- * * main - monty code interpreter
- * @argc: number of arguments
- * @argv: monty file location
- * Return: 0 on success
- */
+* main - monty code interpreter
+* @argc: number of arguments
+* @argv: monty file location
+* Return: 0 on success
+*/
 int main(int argc, char *argv[])
 {
 	char *content;
@@ -23,22 +23,23 @@ int main(int argc, char *argv[])
 	file = fopen(argv[1], "r");
 	bus.file = file;
 	if (!file)
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-	exit(EXIT_FAILURE);
-}
-while (read_line > 0)
-{
-	content = NULL;
-	read_line = getline(&content, &size, file);
-	bus.content = content;
-	counter++;
-	if (read_line > 0)
 	{
-		execute(content, &stack, counter, file);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
 	}
-	free(content);
-}
-free_stack(stack);
-fclose(file);
+	while (read_line > 0)
+	{
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		bus.content = content;
+		counter++;
+		if (read_line > 0)
+		{
+			execute(content, &stack, counter, file);
+		}
+		free(content);
+	}
+	free_stack(stack);
+	fclose(file);
 return (0);
 }
